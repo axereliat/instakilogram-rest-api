b65331ca8347e4d4903663aa02b26e63c7a00af3
mario <axereliat@gmail.com>
2019-03-11 15:49:08 +0200

    News feed implemented

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -36,4 +36,19 @@ router.post('/',
         }
     });
 
+router.get('/newsfeed',
+    checkAuth,
+    async (req, res) => {
+        try {
+            const currentUser = await User.findById(req.user.userId);
+            console.log(currentUser);
+            const posts = (await Post.find({}).populate('author'))
+                .filter(p => currentUser.following.map(x => x.toString()).includes(p.author._id.toString()));
+
+            res.status(200).json(posts);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    });
+
 module.exports = router;
